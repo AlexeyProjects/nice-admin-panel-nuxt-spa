c97ca279c40402cc89bcc6d8476c3a91a269787b
roky <you@example.com>
2022-09-09 18:22:13 +0400

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,9 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: `${'http://178.20.44.99:3060/api/v1/'}`,
   },
-
+  router: {
+    base: './'
+  },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
